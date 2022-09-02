d1c84880f2a6ea7ca05250182aa3ee444cf75a61
Ashish Kumar Singh <aks15o8o2001@gmail.com>
2022-09-03 04:23:29 +0530

    Create OptimizedHCF.cpp

---
+++ OptimizedHCF.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);   
+}
+int main()
+{
+    int a = 98, b = 56;
+    cout<<"GCD of "<<a<<" and "<<b<<" is "<<gcd(a, b);
+    return 0;
+}
